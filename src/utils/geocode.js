620c923e7078f93bb00b041d849ec8a2d39200ec
bindrakesh <rks.bind@gmail.com>
2021-04-17 19:10:24 +0530

    removed unwanted lines

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -14,7 +14,6 @@ const geocode = (address, callback) => {
                 latitude: body.features[0].center[0],
                 longitude: body.features[0].center[1],
                 location: body.features[0].place_name
-
             })
         }
     })
